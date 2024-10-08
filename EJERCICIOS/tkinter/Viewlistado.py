3caef1a5a1a0757c8128b0e777f783fa7131c95a
Nivrox <nicopro.git@gmail.com>
2024-10-07 23:09:24 -0300

    Tkinter Modificaciones expo

--- EJERCICIOS/tkinter/Viewlistado.py
+++ EJERCICIOS/tkinter/Viewlistado.py
@@ -5,6 +5,7 @@ from ConexionBD import *
 def abrir_ventana_listado():
     root = tk.Toplevel()
     root.title("Listado de personas")
+   
 
     style = ttk.Style()
     style.configure("BigFont.TRadiobutton", font=("Helvetica", 14))
